2dafc0886f1eea44b130834fa0ad5d3cf72d326e
Jose Ajcu <jose.ajcu@grupopublimovil.com>
2025-01-29 12:01:42 -0600

    first commit

---
+++ pages/hello.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css";
+
+export default function HomePage() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World</h1>
+    
+    </div>
+  )
+}
