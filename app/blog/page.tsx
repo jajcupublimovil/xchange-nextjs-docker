e13b7a4c421826d9aba17a535197384e56de495f
Jose Ajcu <jose.ajcu@grupopublimovil.com>
2025-01-29 16:08:15 -0600

    Up1.0.4

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,12 @@
-import BlogPost from '@/components/blog/BlogPost'
+//import BlogPost from '@/components/blog/BlogPost'
 import Layout from '@/components/layout/Layout'
 
 export default function Blog() {
     return (
         <>
-            <Layout>
-                <BlogPost showItem={6} style={1} showPagination />
-            </Layout>
+            <div>
+                <h1>Blog</h1>
+            </div>
         </>
     )
 }
